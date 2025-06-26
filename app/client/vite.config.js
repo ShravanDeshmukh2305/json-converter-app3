4160eb0de9c1ea6d446f3dc3d0fee6553ae7eac9
ShravanDeshmukh2305 <shravandeshmukh2305@gmail.com>
2025-06-26 15:13:40 +0530

    Fixed build system

--- app/client/vite.config.js
+++ app/client/vite.config.js
@@ -1,10 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  server: {
+    open: true
+  }
 })
+
 
 
 // import { defineConfig } from 'vite';
